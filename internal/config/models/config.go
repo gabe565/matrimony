962dce944737ff7f1e1ca08a241e03ed2dbe0099
Gabe Cook <gabe565@gmail.com>
2022-02-08 21:54:46 -0600

    :wrench: Move sections config key

--- internal/config/models/config.go
+++ internal/config/models/config.go
@@ -2,9 +2,9 @@ package models
 
 type Config struct {
 	EventInfo EventInfo     `yaml:"info" json:"info"`
-	Sections  []Section     `yaml:"sections" json:"sections"`
 	Privacy   Privacy       `yaml:"privacy" json:"-"`
 	Partners  []PartyMember `yaml:"partners" json:"partners"`
 	Admin     Admin         `yaml:"admin" json:"-"`
+	Sections  []Section     `yaml:"sections" json:"sections"`
 	Party     Party         `yaml:"party" json:"party"`
 }
