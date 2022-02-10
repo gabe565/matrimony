e8ce6615f3030e78850c35222abc3b5a24c91a5e
Gabe Cook <gabe565@gmail.com>
2022-02-09 21:42:25 -0600

    :wrench: Tweak event date config

--- internal/config/models/event.go
+++ internal/config/models/event.go
@@ -2,17 +2,12 @@ package models
 
 import "time"
 
-type EventDate struct {
-	Show  bool      `yaml:"show" json:"show"`
-	Value time.Time `yaml:"value" json:"value"`
-}
-
 type Event struct {
-	Title       string    `yaml:"title" json:"title"`
-	Description string    `yaml:"description" json:"description"`
-	Location    string    `yaml:"location" json:"location"`
-	ShowMap     bool      `yaml:"showMap" json:"showMap"`
-	DressCode   string    `yaml:"dressCode" json:"dressCode"`
-	Start       EventDate `yaml:"start" json:"start"`
-	End         EventDate `yaml:"end" json:"end"`
+	Title       string     `yaml:"title" json:"title"`
+	Description string     `yaml:"description" json:"description"`
+	Location    string     `yaml:"location" json:"location"`
+	ShowMap     bool       `yaml:"showMap" json:"showMap"`
+	DressCode   string     `yaml:"dressCode" json:"dressCode"`
+	Start       *time.Time `yaml:"start" json:"start"`
+	End         *time.Time `yaml:"end" json:"end"`
 }
