9f6e516d8ea293eab6fc9f95ee306ddfaa126d7b
Gabe Cook <gabe565@gmail.com>
2022-02-10 00:23:46 -0600

    :bug: Fix scroll behavior

--- frontend/src/plugins/router.js
+++ frontend/src/plugins/router.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import NotFound from "../views/NotFound.vue";
+import { waitForElement } from "../util/waitForElement";
 
 const router = [
   { path: "/", component: Home },
@@ -10,4 +11,17 @@ const router = [
 export default createRouter({
   history: createWebHistory(),
   routes: router,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    if (to.hash) {
+      /* eslint-disable-next-line no-async-promise-executor */
+      return new Promise(async (resolve) => {
+        await waitForElement(to.hash);
+        resolve({ el: to.hash });
+      });
+    }
+    return { top: 0 };
+  },
 });
---
+++ frontend/src/util/waitForElement.js
@@ -0,0 +1,19 @@
+export const waitForElement = (selector) =>
+  new Promise((resolve) => {
+    if (document.querySelector(selector)) {
+      resolve(document.querySelector(selector));
+      return;
+    }
+
+    const observer = new MutationObserver(() => {
+      if (document.querySelector(selector)) {
+        resolve(document.querySelector(selector));
+        observer.disconnect();
+      }
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  });
