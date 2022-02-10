b61978aeeffe14dc77cacc02c87071556d4354e2
Gabe Cook <gabe565@gmail.com>
2022-02-10 00:23:26 -0600

    :sparkles: Add 404 page

--- frontend/src/plugins/router.js
+++ frontend/src/plugins/router.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import NotFound from "../views/NotFound.vue";
 
 const router = [
   { path: "/", component: Home },
+  { path: "/:pathMatch(.*)*", component: NotFound },
 ];
 
 export default createRouter({
---
+++ frontend/src/views/NotFound.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    class="h-screen container mx-auto flex flex-col justify-center text-center pt-52 lg:pt-0"
+  >
+    <section
+      class="lg:mt-40 text-slate-600 dark:text-slate-400 bg-white dark:bg-slate-900 body-font"
+    >
+      <div class="container px-5 py-24 mx-auto">
+        <div class="text-center mb-4">
+          <h1
+            class="text-4xl font-script title-font mb-2 text-slate-900 dark:text-white"
+          >
+            404 Not Found
+          </h1>
+        </div>
+        <span>
+          I hate to be the bearer of bad news, but the requested file could not
+          be located.
+          <router-link to="/" class="underline"
+            >Click here to go home.</router-link
+          >
+        </span>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+};
+</script>
