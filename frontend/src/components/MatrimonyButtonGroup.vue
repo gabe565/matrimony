137a84fd40eded83381d334bc37ed5b58b8823ae
Gabe Cook <gabe565@gmail.com>
2022-02-12 02:40:40 -0600

    :sparkles: Add button group component

---
+++ frontend/src/components/MatrimonyButtonGroup.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="root">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MatrimonyButtonGroup",
+};
+</script>
+
+<style scoped lang="scss">
+.root > {
+  :slotted(*) {
+    &:first-child {
+      @apply rounded-r-none border-l;
+    }
+
+    @apply border-l-0;
+
+    &:last-child {
+      @apply rounded-l-none;
+    }
+
+    &:only-child {
+      @apply rounded-lg;
+    }
+  }
+}
+</style>
--- internal/config/models/event.go
+++ internal/config/models/event.go
@@ -2,10 +2,15 @@ package models
 
 import "time"
 
+type Button struct {
+	Type string `yaml:"type" json:"type"`
+}
+
 type Event struct {
 	Title       string     `yaml:"title" json:"title"`
 	Description string     `yaml:"description" json:"description"`
 	Location    string     `yaml:"location" json:"location"`
+	Buttons     []Button   `yaml:"buttons,omitempty" json:"buttons,omitempty"`
 	DressCode   string     `yaml:"dressCode" json:"dressCode"`
 	Start       *time.Time `yaml:"start" json:"start"`
 }
