751a7e4d7ed2f23fbe36b933063a239f5423ab6a
Gabe Cook <gabe565@gmail.com>
2022-02-13 20:32:45 -0600

    :sparkles: Add "to" prop to MatrimonyButton

--- frontend/src/components/MatrimonyButton.vue
+++ frontend/src/components/MatrimonyButton.vue
@@ -1,8 +1,9 @@
 <template>
-  <a
-    :href="href"
-    class="inline-flex items-center py-2 px-4 text-sm font-medium text-slate-900 bg-accent-100 rounded-lg border border-slate-200 hover:bg-slate-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-slate-800 dark:text-slate-400 dark:border-slate-600 dark:hover:text-white dark:hover:bg-slate-700"
-  >
+  <router-link v-if="to" :to="to">
+    <slot />
+    <slot name="icon" class="ml-2" />
+  </router-link>
+  <a v-else :href="href">
     <slot />
     <slot name="icon" class="ml-2" />
   </a>
@@ -10,6 +11,13 @@
 
 <script setup>
 defineProps({
+  to: { type: String, default: "" },
   href: { type: String, default: "" },
 });
 </script>
+
+<style scoped lang="scss">
+a {
+  @apply inline-flex items-center py-2 px-4 text-sm font-medium text-slate-900 bg-accent-100 rounded-lg border border-slate-200 hover:bg-slate-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-slate-800 dark:text-slate-400 dark:border-slate-600 dark:hover:text-white dark:hover:bg-slate-700;
+}
+</style>
