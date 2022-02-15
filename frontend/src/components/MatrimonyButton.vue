498dcd0e00e8d11a6e94ba7c1d0f0ea03a36de01
Gabe Cook <gabe565@gmail.com>
2022-02-14 23:51:08 -0600

    :lipstick: Increase dark button contrast

--- frontend/src/components/MatrimonyButton.vue
+++ frontend/src/components/MatrimonyButton.vue
@@ -18,6 +18,6 @@ defineProps({
 
 <style scoped lang="scss">
 a {
-  @apply inline-flex items-center py-2 px-4 text-sm font-medium text-slate-900 bg-accent-100 rounded-lg border border-slate-200 hover:bg-slate-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-slate-800 dark:text-slate-400 dark:border-slate-600 dark:hover:text-white dark:hover:bg-slate-700;
+  @apply inline-flex items-center py-2 px-4 text-sm font-medium text-slate-900 bg-accent-100 rounded-lg border border-slate-200 hover:bg-slate-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-slate-800 dark:text-slate-300 dark:border-slate-600 dark:hover:text-white dark:hover:bg-slate-700;
 }
 </style>
