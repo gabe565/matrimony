9f3e0bc61e738319e92c5b84b3bd89cf7b91299d
Gabe Cook <gabe565@gmail.com>
2022-02-17 01:12:51 -0600

    :sparkles: Add back button to moments page

--- frontend/src/views/Moments.vue
+++ frontend/src/views/Moments.vue
@@ -3,8 +3,8 @@
     <section
       class="lg:mt-40 text-slate-600 dark:text-slate-400 bg-white dark:bg-slate-900 body-font rounded-t-2xl"
     >
-      <div class="container px-5 py-24 mx-auto">
-        <div class="text-center mb-4">
+      <div class="container px-5 py-24 mx-auto flex flex-col">
+        <div class="text-center mb-4 -order-1">
           <h1
             class="text-4xl font-script title-font mb-2 text-slate-900 dark:text-white"
           >
@@ -19,6 +19,16 @@
             />
           </div>
         </div>
+        <matrimony-button
+          class="sticky top-44 lg:top-[5.5rem] -order-1 self-start"
+          to="/"
+          @click.prevent="$router.back()"
+        >
+          <template #prefix-icon>
+            <font-awesome-icon :icon="['far', 'arrow-left']" class="mr-2" />
+          </template>
+          Back
+        </matrimony-button>
       </div>
     </section>
   </div>
@@ -27,6 +37,7 @@
 <script setup>
 import { ref } from "vue";
 import LazyImage from "../components/LazyImage.vue";
+import MatrimonyButton from "../components/MatrimonyButton.vue";
 
 const files = ref([]);
 fetch("/api/moments").then(async (r) => {
