36b004e48579856f3b0f402fe9b46c4b72fb7591
Gabe Cook <gabe565@gmail.com>
2022-02-17 19:01:30 -0600

    :bento: Tweak image section heights

--- frontend/src/components/Sections/ImageSection.vue
+++ frontend/src/components/Sections/ImageSection.vue
@@ -2,7 +2,7 @@
   <lazy-image
     :src="src"
     :alt="alt"
-    class="h-[15rem] md:h-[30rem] w-full object-cover pointer-events-none"
+    class="h-[20rem] md:h-[30rem] lg:h-[40rem] w-full object-cover pointer-events-none"
   />
 </template>
 
