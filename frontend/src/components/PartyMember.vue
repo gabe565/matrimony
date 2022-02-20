1b43bd3a155412f60850f41541008cd8e921ad98
Gabe Cook <gabe565@gmail.com>
2022-02-20 16:16:02 -0600

    :sparkles: Add party member modal with bio

--- frontend/src/components/PartyMember.vue
+++ frontend/src/components/PartyMember.vue
@@ -1,29 +1,62 @@
 <template>
   <div class="p-4 hover:scale-125 duration-300 ease-out">
-    <div class="flex flex-shrink flex-col items-center text-center">
-      <lazy-image
-        :src="member.image"
-        :alt="`${member.first} ${member.last} Photo`"
-        class="flex-shrink-0 rounded-lg h-40 sm:h-60 xl:h-72 w-40 sm:w-60 xl:w-72 mb-2 pointer-events-none"
-      />
-      <h2
-        class="title-font font-medium text-lg text-slate-900 dark:text-slate-400"
-      >
-        {{ member.first }} {{ member.last }}
-      </h2>
-      <h3 class="text-slate-600 dark:text-slate-500 mb-3">
-        {{ member.title }}
-      </h3>
-    </div>
+    <matrimony-modal>
+      <template #activator="{ on }">
+        <div
+          class="flex flex-shrink flex-col items-center text-center cursor-pointer"
+          v-on="on"
+        >
+          <lazy-image
+            :src="member.image"
+            :alt="`Photo of ${member.first} ${member.last}`"
+            class="flex-shrink-0 rounded-lg h-40 sm:h-60 xl:h-72 w-40 sm:w-60 xl:w-72 mb-2 pointer-events-none"
+          />
+          <h2
+            class="title-font font-medium text-lg text-slate-900 dark:text-slate-400"
+          >
+            {{ member.first }} {{ member.last }}
+          </h2>
+          <h3 class="text-slate-600 dark:text-slate-500 mb-3">
+            {{ member.title }}
+          </h3>
+        </div>
+      </template>
+
+      <template #title>
+        <div>{{ member.first }} {{ member.last }}</div>
+        <h3 class="font-medium text-lg text-slate-600 dark:text-slate-500">
+          {{ member.title }}
+        </h3>
+      </template>
+
+      <template #body>
+        <div class="flex flex-shrink flex-col md:flex-row items-center">
+          <lazy-image
+            :src="member.image"
+            :alt="`Photo of ${member.first} ${member.last}`"
+            class="flex-shrink-0 max-w-full rounded-lg h-80 w-80 mb-2 pointer-events-none"
+          />
+
+          <matrimony-markdown :source="textContent" class="px-5" />
+        </div>
+      </template>
+    </matrimony-modal>
   </div>
 </template>
 
 <script setup>
+import { computed } from "vue";
 import LazyImage from "./LazyImage.vue";
+import MatrimonyModal from "./MatrimonyModal.vue";
+import MatrimonyMarkdown from "./MatrimonyMarkdown.vue";
 
-defineProps({
+const props = defineProps({
   member: { type: Object, default: () => ({}) },
 });
+
+const textContent = computed(() =>
+  props.member.text?.content ? `> ${props.member.text.content}` : ""
+);
 </script>
 
 <script>
--- internal/config/models/party_member.go
+++ internal/config/models/party_member.go
@@ -1,12 +1,13 @@
 package models
 
 type PartyMember struct {
-	First string `yaml:"first" json:"first"`
-	Last  string `yaml:"last" json:"last"`
-	Title string `yaml:"title" json:"title"`
-	Email string `yaml:"email,omitempty" json:"email,omitempty"`
-	Info  string `yaml:"info,omitempty" json:"info,omitempty"`
-	Image string `yaml:"image,omitempty" json:"image,omitempty"`
+	First string       `yaml:"first" json:"first"`
+	Last  string       `yaml:"last" json:"last"`
+	Title string       `yaml:"title" json:"title"`
+	Email string       `yaml:"email,omitempty" json:"email,omitempty"`
+	Info  string       `yaml:"info,omitempty" json:"info,omitempty"`
+	Image string       `yaml:"image,omitempty" json:"image,omitempty"`
+	Text  *TextSection `yaml:"text,omitempty" json:"text,omitempty"`
 }
 
 func (member PartyMember) Fullname() string {
