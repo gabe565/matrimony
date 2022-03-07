5d03e79a5c50b9a517078da9298037c8eb63ec2e
Gabe Cook <gabe565@gmail.com>
2022-03-07 14:14:11 -0600

    :alien: Replace ioutil with io

--- internal/server/middleware/log_body.go
+++ internal/server/middleware/log_body.go
@@ -2,19 +2,19 @@ package middleware
 
 import (
 	"bytes"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 )
 
 func LogBody(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		b, err := ioutil.ReadAll(r.Body)
+		b, err := io.ReadAll(r.Body)
 		if err != nil {
 			panic(err)
 		}
 		log.Println("Request body: " + string(b))
-		r.Body = ioutil.NopCloser(bytes.NewReader(b))
+		r.Body = io.NopCloser(bytes.NewReader(b))
 		next.ServeHTTP(w, r)
 	})
 }
