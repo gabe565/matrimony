9da3ce1e9e89d9815f4ff9f476a781af6ec3f09b
Gabe Cook <gabe565@gmail.com>
2022-07-29 16:45:27 -0500

    :sparkles: Add support for lazy-image fallback icons

--- frontend/src/components/LazyImage.vue
+++ frontend/src/components/LazyImage.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="relative overflow-hidden" :class="[bgColor]">
     <img
+      v-if="src"
       v-lazy="{ src, lifecycle }"
       :alt="alt"
       class="h-full w-full duration-300"
       :class="[contentFit]"
     />
     <div
-      v-if="loading"
+      v-if="loading || !src"
       class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
     >
       <font-awesome-icon
+        v-if="loading"
         :icon="['fad', 'spinner-third']"
         class="ml-2 text-4xl dark:text-white animate-spin"
+      />
+      <font-awesome-icon
+        v-else-if="fallbackIcon"
+        :icon="fallbackIcon"
+        class="ml-2"
+        :class="fallbackClass"
       />
     </div>
   </div>
@@ -21,14 +29,19 @@
 <script setup>
 import { ref } from "vue";
 
-defineProps({
+const props = defineProps({
   src: { type: String, default: "" },
   alt: { type: String, default: "" },
   bgColor: { type: String, default: "bg-slate-100 dark:bg-slate-800" },
   contentFit: { type: String, default: "object-cover object-center" },
+  fallbackIcon: { type: Array, default: () => [] },
+  fallbackClass: {
+    type: [String, Array, Object],
+    default: "text-7xl dark:text-white",
+  },
 });
 
-const loading = ref(true);
+const loading = ref(props.src !== "");
 const lifecycle = {
   loading: () => {
     loading.value = true;
